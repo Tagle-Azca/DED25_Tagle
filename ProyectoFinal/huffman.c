c0da240c38e0f04a330c72d81306929eb3da709c
Isaian000 <isaianag005@gmail.com>
2025-05-17 10:56:07 -0600

    finally that compile

--- ProyectoFinal/huffman.c
+++ ProyectoFinal/huffman.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "../adt_map/map.h"
+#include "../adt_pq/pq.h"
+
+
 #define MAX_TREE_HT 100
 #define CHAR_SET_SIZE 256
 
@@ -12,171 +16,132 @@ typedef struct Node
     struct Node *left, *right;
 } Node;
 
-typedef struct MinHeap
-{
-    unsigned size;
-    unsigned capacity;
-    Node **array;
-} MinHeap;
-
-Node *newNode(char data, unsigned freq)
-{
-    Node *temp = (Node *)malloc(sizeof(Node));
-    temp->left = temp->right = NULL;
-    temp->data = data;
-    temp->freq = freq;
-    return temp;
+int cmp_nodes(void *a, void *b){
+    Node *n1 = (Node*)a;
+    Node *n2 = (Node*)b;
+    return n1->freq - n2->freq;
 }
 
-MinHeap *createMinHeap(unsigned capacity)
-{
-    MinHeap *minHeap = (MinHeap *)malloc(sizeof(MinHeap));
-    minHeap->size = 0;
-    minHeap->capacity = capacity;
-    minHeap->array = (Node **)malloc(minHeap->capacity * sizeof(Node *));
-    return minHeap;
+int convert_char(void *key){
+    return *(( unsigned char *)key);
 }
 
-void swapNodes(Node **a, Node **b)
-{
-    Node *t = *a;
-    *a = *b;
-    *b = t;
+boolean compare_char(void *a, void *b){
+    return *((unsigned char *)a) == *((unsigned char *)b);
 }
 
-void minHeapify(MinHeap *minHeap, int idx)
-{
-    int smallest = idx;
-    int left = 2 * idx + 1;
-    int right = 2 * idx + 2;
+map *getFrequencies(const char *text){
+    map *freqMap = map_create(256, convert_char, compare_char);
+    for (int i = 0; text[i] != '\0'; i++){
+        char c = text[i];
+        char *key = malloc(sizeof(char));
+        *key = c;
 
-    if (left < minHeap->size && minHeap->array[left]->freq < minHeap->array[smallest]->freq)
-        smallest = left;
-
-    if (right < minHeap->size && minHeap->array[right]->freq < minHeap->array[smallest]->freq)
-        smallest = right;
-
-    if (smallest != idx)
-    {
-        swapNodes(&minHeap->array[smallest], &minHeap->array[idx]);
-        minHeapify(minHeap, smallest);
+        if (map_contains(freqMap, key))
+        {
+            int *count;
+            count = (int*)map_get(freqMap, key);
+            (*count)++;
+            free(key);
+        } else{
+            int *count;
+            count = malloc(sizeof(int));
+            *count=1;
+            map_put(freqMap, key, count);
+        }
+        
     }
+    return freqMap;
+    
 }
 
-int isSizeOne(MinHeap *minHeap)
+Node *buildHuffmanTree(map *freqMap)
 {
-    return (minHeap->size == 1);
-}
-
-Node *extractMin(MinHeap *minHeap)
-{
-    Node *temp = minHeap->array[0];
-    minHeap->array[0] = minHeap->array[--minHeap->size];
-    minHeapify(minHeap, 0);
-    return temp;
-}
-
-void insertMinHeap(MinHeap *minHeap, Node *node)
-{
-    ++minHeap->size;
-    int i = minHeap->size - 1;
-    while (i && node->freq < minHeap->array[(i - 1) / 2]->freq)
-    {
-        minHeap->array[i] = minHeap->array[(i - 1) / 2];
-        i = (i - 1) / 2;
-    }
-    minHeap->array[i] = node;
-}
-
-void buildMinHeap(MinHeap *minHeap)
-{
-    int n = minHeap->size - 1;
-    for (int i = (n - 1) / 2; i >= 0; --i)
-        minHeapify(minHeap, i);
-}
-
-int isLeaf(Node *node)
-{
-    return !(node->left) && !(node->right);
-}
-
-MinHeap *createAndBuildMinHeap(char data[], int freq[], int size)
-{
-    MinHeap *minHeap = createMinHeap(size);
-    for (int i = 0; i < size; ++i)
-        minHeap->array[i] = newNode(data[i], freq[i]);
-    minHeap->size = size;
-    buildMinHeap(minHeap);
-    return minHeap;
-}
-
-Node *buildHuffmanTree(char data[], int freq[], int size)
-{
+    pq *queue = pq_create(256, cmp_nodes);
     Node *left, *right, *top;
-    MinHeap *minHeap = createAndBuildMinHeap(data, freq, size);
-
-    while (!isSizeOne(minHeap))
-    {
-        left = extractMin(minHeap);
-        right = extractMin(minHeap);
-        top = newNode('$', left->freq + right->freq);
-        top->left = left;
-        top->right = right;
-        insertMinHeap(minHeap, top);
-    }
-
-    return extractMin(minHeap);
-}
-
-void storeCodes(Node *root, int arr[], int top, char *codes[CHAR_SET_SIZE])
-{
-    if (root->left)
-    {
-        arr[top] = 0;
-        storeCodes(root->left, arr, top + 1, codes);
-    }
-
-    if (root->right)
-    {
-        arr[top] = 1;
-        storeCodes(root->right, arr, top + 1, codes);
-    }
-
-    if (isLeaf(root))
-    {
-        codes[(unsigned char)root->data] = (char *)malloc(top + 1);
-        for (int i = 0; i < top; i++)
-            codes[(unsigned char)root->data][i] = arr[i] + '0';
-        codes[(unsigned char)root->data][top] = '\0';
-    }
-}
-
-int getFrequencies(const char *text, char *characters, int *frequencies)
-{
-    int freq[CHAR_SET_SIZE] = {0};
-    int length = strlen(text);
-    for (int i = 0; i < length; i++)
-        freq[(unsigned char)text[i]]++;
-
-    int count = 0;
+    
     for (int i = 0; i < CHAR_SET_SIZE; i++)
     {
-        if (freq[i])
+        char c = i;
+        if (map_contains(freqMap, &c))
         {
-            characters[count] = (char)i;
-            frequencies[count] = freq[i];
-            count++;
+            int *freq = (int*)map_get(freqMap, &c);
+            Node *n = malloc(sizeof(Node));
+            n->data = c;
+            n->left = NULL;
+            n->right = NULL;
+            n->freq = *freq;
+            pq_offer(queue, n);
         }
     }
+    
 
-    return count;
+    while (pq_size(queue) > 1)
+    {
+        left = pq_poll(queue);
+        right = pq_poll(queue);
+
+        top = malloc(sizeof(Node));
+        top->data = '\0';
+        top->left = left;
+        top->right = right;
+        top->freq = left->freq + right->freq;
+
+        pq_offer(queue, top);
+    }
+
+    return pq_poll(queue);
 }
 
-void compressText(const char *text, char *codes[CHAR_SET_SIZE], char *output)
+void storeCodes(Node *root, char *path, map *codeMap)
 {
-    output[0] = '\0';
-    for (int i = 0; text[i]; i++)
-        strcat(output, codes[(unsigned char)text[i]]);
+    if (root == NULL)
+    {
+        /* code */
+        return;
+    }
+    
+    if (root->left == NULL && root->right == NULL)
+    {
+        char *key = malloc(sizeof(char));
+        *key = root->data;
+        char *code = malloc(strlen(path)+1);
+        strcpy(code, path);
+        map_put(codeMap, key, code);
+        return;
+    }
+
+    char leftpath[CHAR_SET_SIZE], rightpath[CHAR_SET_SIZE];
+    int len = strlen(path);
+    for (int i = 0; i < len; i++)
+    {
+        leftpath[i] = path[i];
+    }
+    leftpath[len] = '0';
+    leftpath[len + 1] = '\0';
+    storeCodes(root->left, leftpath, codeMap);
+
+    int len2 = strlen(path);
+    for (int i = 0; i < len2; i++)
+    {
+        rightpath[i] = path[i];
+    }
+    rightpath[len2] = '1';
+    rightpath[len2 + 1] = '\0';
+    storeCodes(root->right, rightpath, codeMap);
+}
+
+char *compressText(const char *text, map *codeMap) {
+    char *compressed = malloc(strlen(text) * 10);
+    compressed[0] = '\0';
+
+    for (int i = 0; text[i]; i++) {
+        char c = text[i];
+        char *code = (char *)map_get(codeMap, &c);
+        strcat(compressed, code);
+    }
+
+    return compressed;
 }
 
 void decompressText(Node *root, const char *compressed)
@@ -185,13 +150,14 @@ void decompressText(Node *root, const char *compressed)
     printf("Texto descomprimido: ");
     for (int i = 0; compressed[i]; i++)
     {
-        if (compressed[i] == '0')
+        if (compressed[i] == '0'){
             curr = curr->left;
-        else
+        } else{
             curr = curr->right;
+        }
+            
 
-        if (isLeaf(curr))
-        {
+        if (curr->left == NULL && curr->right == NULL) {
             putchar(curr->data);
             curr = root;
         }
@@ -202,28 +168,29 @@ void decompressText(Node *root, const char *compressed)
 int main()
 {
     const char *text = "The sun is a huge ball of gases. It has a huge diameter. It is so huge that it can hold millions of planets inside it. The Sun is mainly made up of hydrogen and helium gas.";
-    char characters[CHAR_SET_SIZE];
-    int frequencies[CHAR_SET_SIZE];
+    /* char characters[CHAR_SET_SIZE];
+    int frequencies[CHAR_SET_SIZE]; */
 
-    int size = getFrequencies(text, characters, frequencies);
+    map *freqMap = getFrequencies(text);
+    Node *root = buildHuffmanTree(freqMap);
 
-    Node *root = buildHuffmanTree(characters, frequencies, size);
+    /*int arr[MAX_TREE_HT], top = 0;
+    char *codes[CHAR_SET_SIZE] = {0};*/
 
-    int arr[MAX_TREE_HT], top = 0;
-    char *codes[CHAR_SET_SIZE] = {0};
-
-    storeCodes(root, arr, top, codes);
+    map *codeMap = map_create(CHAR_SET_SIZE, convert_char, compare_char);
+    storeCodes(root, "", codeMap);
 
     printf("Tabla de códigos de Huffman:\n");
-    for (int i = 0; i < CHAR_SET_SIZE; i++)
-    {
-        if (codes[i])
-            printf("'%c': %s\n", i, codes[i]);
+    for (int i = 0; i < 256; i++) {
+        char c = i;
+        if (map_contains(codeMap, &c)) {
+            char *code = (char *)map_get(codeMap, &c);
+            printf("'%c': %s\n", c, code);
+        }
     }
 
-    char compressed[10000];
-    compressText(text, codes, compressed);
-
+  
+    char *compressed = compressText(text, codeMap);
     printf("\nTexto comprimido:\n%s\n\n", compressed);
 
     decompressText(root, compressed);
